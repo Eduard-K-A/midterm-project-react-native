31f44d90bc498d744c8d3c528cc9e367d5e6b77c
Eduard <eduard_king_anterola@dlsl.edu.ph>
2026-02-20 00:40:41 +0800

    feat: add typography constants

---
+++ src/theme/typography.ts
@@ -0,0 +1,33 @@
+export const typography = {
+  headingXL: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: '700' as const,
+  },
+  headingLG: {
+    fontSize: 20,
+    lineHeight: 28,
+    fontWeight: '700' as const,
+  },
+  headingMD: {
+    fontSize: 18,
+    lineHeight: 24,
+    fontWeight: '600' as const,
+  },
+  bodyLG: {
+    fontSize: 16,
+    lineHeight: 22,
+    fontWeight: '400' as const,
+  },
+  bodyMD: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: '400' as const,
+  },
+  caption: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: '400' as const,
+  },
+};
+
